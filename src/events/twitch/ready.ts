2ce6668806b122b033d40851956dd04b78e1f3c6
suzuki3jp <99058480+suzuki3jp@users.noreply.github.com>
2022-12-29 16:28:35 +0900

    update ready.ts

--- src/events/twitch/ready.ts
+++ src/events/twitch/ready.ts
@@ -1,5 +1,5 @@
 import type { Logger } from '@suzuki3jp/utils';
-import type { TwitchClient } from '../../TwitchClient/index';
+import type { TwitchClient } from '@suzuki3jp/twitch.js';
 
 export const twitchReady = (client: TwitchClient, logger: Logger) => {
     logger.system('twitch client is ready.');
