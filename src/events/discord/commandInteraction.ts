04b89df70b6f32c522a260c72359f53f2946f109
suzuki3jp <99058480+suzuki3jp@users.noreply.github.com>
2023-01-10 22:46:23 +0900

    update discord slash command. "panel" save manage command panel message id to settings.json

--- src/events/discord/commandInteraction.ts
+++ src/events/discord/commandInteraction.ts
@@ -1,13 +1,15 @@
 import { Client, ChatInputCommandInteraction, ButtonBuilder } from 'discord.js';
 import { pageManagerActionRow, commandManagerActionRow } from '../../data/Components';
 import { createCommandPanelEmbeds } from '../../utils/Embed';
-import { readFileSync } from 'fs';
+import { readFileSync, writeFileSync } from 'fs';
 import { resolve } from 'path';
 
 const settingsPath = resolve(__dirname, '../../data/settings.json');
 
-export const commandInteraction = (client: Client, interaction: ChatInputCommandInteraction) => {
-    const settings: { discord: { modRoleId: string } } = JSON.parse(readFileSync(settingsPath, 'utf-8'));
+export const commandInteraction = async (client: Client, interaction: ChatInputCommandInteraction) => {
+    const settings: { discord: { modRoleId: string; manageCommandPanelId: string | null } } = JSON.parse(
+        readFileSync(settingsPath, 'utf-8')
+    );
     const member = interaction.guild?.members.resolve(interaction.user);
 
     if (member?.roles.cache.has(settings.discord.modRoleId)) {
@@ -15,11 +17,14 @@ export const commandInteraction = (client: Client, interaction: ChatInputCommand
             if (pageManagerActionRow.components[0] instanceof ButtonBuilder) {
                 pageManagerActionRow.components[0].setDisabled(true);
             }
-            interaction.channel?.send({
+            const panel = await interaction.channel?.send({
                 embeds: [createCommandPanelEmbeds()[0]],
                 // @ts-ignore
                 components: [pageManagerActionRow, commandManagerActionRow],
             });
+            settings.discord.manageCommandPanelId = panel?.id ?? null;
+            const writeData = JSON.stringify(settings, null, '\t');
+            writeFileSync(settingsPath, writeData, 'utf-8');
         } else if (interaction.options.getSubcommand() === 'template') {
         }
     } else {
