877be7e42f73a77e1a3b265c4208e7e7bb59d7e8
suzuki3jp <99058480+suzuki3jp@users.noreply.github.com>
2023-01-12 19:13:29 +0900

    refactoring the discord button interactions.

---
+++ src/class/DiscordButton.ts
@@ -0,0 +1,147 @@
+import { Client, ButtonInteraction, GuildMember, MessageButton, Message } from 'discord.js';
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
+import { ComponentCustomIds, addTemplateModal } from '../data/Components';
+import { isFirstPageByFooter, isLastPageByFooter, createCommandPanelEmbeds, currentPage } from '../utils/Embed';
+
+const settingsPath = resolve(__dirname, '../data/settings.json');
+
+export class DiscordButton {
+    public client: Client;
+    public interaction: ButtonInteraction;
+    public member: GuildMember | null;
+    public type: ButtonTypes;
+    public customId: string;
+
+    constructor(client: Client, interaction: ButtonInteraction) {
+        this.client = client;
+        this.interaction = interaction;
+        this.member = this.interaction.guild?.members.resolve(this.interaction.user) ?? null;
+        this.customId = this.interaction.customId;
+        this.type = this.buttonType();
+    }
+
+    buttonType(): ButtonTypes {
+        if (this.customId === ComponentCustomIds.button.add) return 'commandAdd';
+        if (this.customId === ComponentCustomIds.button.edit) return 'commandEdit';
+        if (this.customId === ComponentCustomIds.button.remove) return 'commandRemove';
+        if (this.customId === ComponentCustomIds.button.next) return 'panelNext';
+        if (this.customId === ComponentCustomIds.button.previous) return 'panelPrevious';
+        if (this.customId === ComponentCustomIds.button.addTemplate) return 'templateAdd';
+        if (this.customId.startsWith(ComponentCustomIds.button.commandTemplate)) return 'template';
+        return 'site';
+    }
+
+    isMod(): boolean {
+        const settings: { discord: { modRoleId: string } } = JSON.parse(readFileSync(settingsPath, 'utf-8'));
+        return this.member?.roles.cache.has(settings.discord.modRoleId) ?? false;
+    }
+
+    next() {
+        const manageCommandPanel = this.interaction.message;
+        const embeds = manageCommandPanel.embeds;
+        const components = manageCommandPanel.components;
+        if (!components || !isMessage(manageCommandPanel)) return;
+        const isFirstPage = isFirstPageByFooter(this.interaction.message.embeds[0]);
+        const currentPageNumber = currentPage(this.interaction.message.embeds[0]);
+        const newPage = createCommandPanelEmbeds()[currentPageNumber];
+        if (isFirstPage) {
+            // 最初のページから次のページに遷移したとき、戻るボタンの無効化を解除
+            if (components[0].components[0] instanceof MessageButton) {
+                components[0].components[0].setDisabled(false);
+            }
+            embeds[0] = newPage;
+            manageCommandPanel.edit({
+                embeds,
+                // @ts-ignore
+                components,
+            });
+            this.interaction.deferUpdate();
+        } else if (isLastPageByFooter(newPage)) {
+            // 最後のページに遷移したとき、次へボタンを無効化
+            if (components[0].components[1] instanceof MessageButton) {
+                components[0].components[1].setDisabled(true);
+            }
+            embeds[0] = newPage;
+            manageCommandPanel.edit({
+                embeds,
+                // @ts-ignore
+                components,
+            });
+            this.interaction.deferUpdate();
+        } else {
+            embeds[0] = newPage;
+            manageCommandPanel.edit({
+                embeds,
+                // @ts-ignore
+                components,
+            });
+            this.interaction.deferUpdate();
+        }
+    }
+
+    previous() {
+        const manageCommandPanel = this.interaction.message;
+        const embeds = manageCommandPanel.embeds;
+        const components = manageCommandPanel.components;
+        if (!components || !isMessage(manageCommandPanel)) return;
+        const isLastPage = isLastPageByFooter(this.interaction.message.embeds[0]);
+        const currentPageNumber = currentPage(this.interaction.message.embeds[0]);
+        const newPage = createCommandPanelEmbeds()[currentPageNumber - 2];
+        if (isLastPage) {
+            // 最後のページから戻ったときに次へボタンを有効化する
+            if (components[0].components[1] instanceof MessageButton) {
+                components[0].components[1].setDisabled(false);
+            }
+            embeds[0] = newPage;
+            manageCommandPanel.edit({
+                embeds,
+                // @ts-ignore
+                components,
+            });
+            this.interaction.deferUpdate();
+        } else if (isFirstPageByFooter(newPage)) {
+            // 最初のページに戻る時に戻るボタンを無効化する
+            if (components[0].components[0] instanceof MessageButton) {
+                components[0].components[0].setDisabled(true);
+            }
+            embeds[0] = newPage;
+            manageCommandPanel.edit({
+                embeds,
+                // @ts-ignore
+                components,
+            });
+            this.interaction.deferUpdate();
+        } else {
+            embeds[0] = newPage;
+            manageCommandPanel.edit({
+                embeds,
+                // @ts-ignore
+                components,
+            });
+            this.interaction.deferUpdate();
+        }
+    }
+
+    addTemplate() {
+        this.interaction.showModal(addTemplateModal);
+    }
+
+    async reply(content: string, ephemeral?: boolean) {
+        await this.interaction.reply({ content, ephemeral: ephemeral ?? false });
+    }
+}
+
+export type ButtonTypes =
+    | 'commandAdd'
+    | 'commandEdit'
+    | 'commandRemove'
+    | 'panelNext'
+    | 'panelPrevious'
+    | 'templateAdd'
+    | 'template'
+    | 'site';
+
+const isMessage = (data: any): data is Message => {
+    return true;
+};
--- src/events/discord/buttonInteraction.ts
+++ src/events/discord/buttonInteraction.ts
@@ -1,105 +1,21 @@
-import { Message, MessageButton } from 'discord.js';
 import type { Client, ButtonInteraction } from 'discord.js';
-import { ComponentCustomIds, addTemplateModal } from '../../data/Components';
-import { createCommandPanelEmbeds, currentPage, isFirstPageByFooter, isLastPageByFooter } from '../../utils/Embed';
+import { DiscordButton } from '../../class/DiscordButton';
 
 export const buttonInteraction = (client: Client, interaction: ButtonInteraction) => {
-    //コマンドパネルの次へボタン
-    if (interaction.customId === ComponentCustomIds.button.next) {
-        if (interaction.message.embeds[0]) {
-            const manageCommandPanel = interaction.message;
-            const embeds = manageCommandPanel.embeds;
-            const components = manageCommandPanel.components;
-            if (!components || !isMessage(manageCommandPanel)) return;
-            const isFirstPage = isFirstPageByFooter(interaction.message.embeds[0]);
-            const currentPageNumber = currentPage(interaction.message.embeds[0]);
-            const newPage = createCommandPanelEmbeds()[currentPageNumber];
-            if (isFirstPage) {
-                // 最初のページから次のページに遷移したとき、戻るボタンの無効化を解除
-                if (components[0].components[0] instanceof MessageButton) {
-                    components[0].components[0].setDisabled(false);
-                }
-                embeds[0] = newPage;
-                manageCommandPanel.edit({
-                    embeds,
-                    // @ts-ignore
-                    components,
-                });
-                interaction.deferUpdate();
-            } else if (isLastPageByFooter(newPage)) {
-                // 最後のページに遷移したとき、次へボタンを無効化
-                if (components[0].components[1] instanceof MessageButton) {
-                    components[0].components[1].setDisabled(true);
-                }
-                embeds[0] = newPage;
-                manageCommandPanel.edit({
-                    embeds,
-                    // @ts-ignore
-                    components,
-                });
-                interaction.deferUpdate();
-            } else {
-                embeds[0] = newPage;
-                manageCommandPanel.edit({
-                    embeds,
-                    // @ts-ignore
-                    components,
-                });
-                interaction.deferUpdate();
-            }
-        }
-    }
+    const button = new DiscordButton(client, interaction);
 
-    // コマンドパネルの戻るボタン
-    if (interaction.customId === ComponentCustomIds.button.previous) {
-        const manageCommandPanel = interaction.message;
-        const embeds = manageCommandPanel.embeds;
-        const components = manageCommandPanel.components;
-        if (!components || !isMessage(manageCommandPanel)) return;
-        const isLastPage = isLastPageByFooter(interaction.message.embeds[0]);
-        const currentPageNumber = currentPage(interaction.message.embeds[0]);
-        const newPage = createCommandPanelEmbeds()[currentPageNumber - 2];
-        if (isLastPage) {
-            // 最後のページから戻ったときに次へボタンを有効化する
-            if (components[0].components[1] instanceof MessageButton) {
-                components[0].components[1].setDisabled(false);
-            }
-            embeds[0] = newPage;
-            manageCommandPanel.edit({
-                embeds,
-                // @ts-ignore
-                components,
-            });
-            interaction.deferUpdate();
-        } else if (isFirstPageByFooter(newPage)) {
-            // 最初のページに戻る時に戻るボタンを無効化する
-            if (components[0].components[0] instanceof MessageButton) {
-                components[0].components[0].setDisabled(true);
-            }
-            embeds[0] = newPage;
-            manageCommandPanel.edit({
-                embeds,
-                // @ts-ignore
-                components,
-            });
-            interaction.deferUpdate();
-        } else {
-            embeds[0] = newPage;
-            manageCommandPanel.edit({
-                embeds,
-                // @ts-ignore
-                components,
-            });
-            interaction.deferUpdate();
-        }
+    // コマンドパネルページネーション
+    switch (button.type) {
+        case 'panelNext':
+            button.next();
+            break;
+        case 'panelPrevious':
+            button.previous();
+            break;
+        case 'templateAdd':
+            button.addTemplate();
+            break;
+        default:
+            break;
     }
-
-    // テンプレートの追加ボタン
-    if (interaction.customId === ComponentCustomIds.button.addTemplate) {
-        interaction.showModal(addTemplateModal);
-    }
-};
-
-const isMessage = (data: any): data is Message => {
-    return true;
 };
