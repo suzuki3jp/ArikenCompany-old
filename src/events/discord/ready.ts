5a8a499fe1da323b1e09341827372aef90ff7e65
suzuki3jp <99058480+suzuki3jp@users.noreply.github.com>
2023-01-14 04:34:40 +0900

    update discord ready event.

--- src/events/discord/ready.ts
+++ src/events/discord/ready.ts
@@ -3,6 +3,8 @@ import { resolve } from 'path';
 import type { Client } from 'discord.js';
 import type { Logger } from '@suzuki3jp/utils';
 
+import { CommandManager } from '../../class/Command';
+
 import { slashCommands } from '../../data/SlashCommands';
 
 // paths
@@ -10,6 +12,8 @@ const settingsPath = resolve(__dirname, '../../data/settings.json');
 
 export const discordReady = async (client: Client, logger: Logger) => {
     const settings: { discord: { guildId: string } } = JSON.parse(readFileSync(settingsPath, 'utf-8'));
+    new CommandManager().syncCommandPanel(client);
+
     await client.application?.commands.set(slashCommands, settings.discord.guildId);
     logger.system('discord client is ready.');
 };
