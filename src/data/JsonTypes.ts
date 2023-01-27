2d78523629d772f0b3eafd9406a21a0b5645f17b
suzuki3jp <99058480+suzuki3jp@users.noreply.github.com>
2023-01-27 20:56:20 +0900

    refactor: SettingsJsonの型定義にドキュメントコメントを追加

--- src/data/JsonTypes.ts
+++ src/data/JsonTypes.ts
@@ -11,21 +11,73 @@ export type MessageCounterJson = Record<string, number>;
 export type PublicCommandsJson = Record<string, string>;
 
 export interface SettingsJson {
+    /**
+     * discordに関する設定
+     */
     discord: {
+        /**
+         * 管理者ロールID
+         */
         modRoleId: string;
+
+        /**
+         * aricordギルドID
+         */
         guildId: string;
+
+        /**
+         * コマンド管理パネルのチャンネル
+         */
         manageCommandChannelId: string;
+
+        /**
+         * コマンド管理パネルのメッセージID
+         */
         manageCommandPanelId: string | null;
     };
+
+    /**
+     * twitchに関する設定
+     */
     twitch: {
+        /**
+         * ログインしているチャンネル名
+         */
         channelName: string;
+
+        /**
+         * 接続しているチャンネル名
+         */
         channels: string[];
+
+        /**
+         * コマンド管理コマンドの配列
+         */
         manageCommands: string[];
+
+        /**
+         * コマンドが現在有効かどうか
+         */
         command: boolean;
+
+        /**
+         * コマンドのクールタイム時間（秒）
+         */
         cooltime: number;
     };
+
+    /**
+     * APIに関する設定
+     */
     api: {
+        /**
+         * ポート
+         */
         port: number;
+
+        /**
+         * HTTPSを使用するかどうか
+         */
         isSecure: boolean;
     };
 }
