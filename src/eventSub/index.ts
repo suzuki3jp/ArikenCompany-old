e615fb8a8a22564a65cf0c188d2d739122e6181c
suzuki3jp <99058480+suzuki3jp@users.noreply.github.com>
2023-01-29 22:16:24 +0900

    update: EventSubWsListenerのイベントインデックス関数を作成

---
+++ src/eventSub/index.ts
@@ -0,0 +1,4 @@
+import { TwitchClient as Twitch } from '@suzuki3jp/twitch.js';
+import { EventSubWsListener } from '@twurple/eventsub-ws';
+
+export const eventSub = (twitchClient: Twitch, eventSub: EventSubWsListener) => {};
