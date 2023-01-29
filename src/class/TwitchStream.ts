370cadef442fb307b7a7f3b23a9e34b8696e5898
suzuki3jp <99058480+suzuki3jp@users.noreply.github.com>
2023-01-30 00:52:25 +0900

    feat: 配信開始時に所定のチャンネルに通知を送信する fix: #5

---
+++ src/class/TwitchStream.ts
@@ -0,0 +1,64 @@
+import { JST } from '@suzuki3jp/utils';
+import { HelixStream } from '@twurple/api';
+import { EventSubStreamOfflineEvent, EventSubStreamOnlineEvent } from '@twurple/eventsub-base';
+
+import { Base } from './Base';
+import { TwitchUser } from '../data/JsonTypes';
+import { MessageEmbed, TextChannel } from 'discord.js';
+import { APIEmbed } from 'discord-api-types/v9';
+
+export class TwitchStream extends Base {
+    public users: TwitchUser[];
+    public user: TwitchUser | null;
+    public userIndex: number | null;
+    constructor(base: Base, event: EventSubStreamOfflineEvent | EventSubStreamOnlineEvent) {
+        super(base.twitch, base.discord, base.logger);
+        this.users = super.DM.getStreamStatus().users;
+        this.user = null;
+        this.userIndex = null;
+        this.users.forEach((value, index) => {
+            if (value.id !== event.broadcasterId) return;
+            this.userIndex = index;
+            this.user = value;
+        });
+    }
+
+    async turnOnline() {
+        if (!this.userIndex || !this.user) return;
+
+        const channel = await super.discord.channels.fetch(this.user.notificationChannelId);
+        const stream = await super.twitch._api.streams.getStreamByUserId(this.user.id);
+        if (!channel || !stream) return;
+        this._updateData(true);
+        const embeds = this._createOnStreamEmbed(stream);
+        if (channel instanceof TextChannel) {
+            channel.send({ embeds });
+        } else return;
+    }
+
+    async turnOffline() {
+        this._updateData(false);
+    }
+
+    _updateData(isStreaming: boolean) {
+        if (!this.userIndex || !this.user) return;
+        const newUsers = this.users.splice(this.userIndex);
+        this.user.isStreaming = isStreaming;
+        newUsers.push(this.user);
+        this.users = newUsers;
+        super.DM.setStreamStatus({ users: this.users });
+    }
+
+    _createOnStreamEmbed(stream: HelixStream): MessageEmbed[] {
+        const embed: APIEmbed = {
+            title: `${stream.userDisplayName}が配信を開始しました`,
+            url: `https://www.twitch.tv/${stream.userName}`,
+            description: `タイトル: ${stream.title} ゲーム: ${stream.gameName}`,
+            footer: {
+                text: JST.getDateString(),
+            },
+        };
+        // @ts-expect-error
+        return [new MessageEmbed(embed)];
+    }
+}
--- src/eventSub/index.ts
+++ src/eventSub/index.ts
@@ -1,4 +1,15 @@
 import { TwitchClient as Twitch } from '@suzuki3jp/twitch.js';
 import { EventSubWsListener } from '@twurple/eventsub-ws';
 
-export const eventSub = (twitchClient: Twitch, eventSub: EventSubWsListener) => {};
+import { Base } from '../class/Base';
+import { TwitchStream } from '../class/TwitchStream';
+
+export const eventSub = (base: Base, eventSub: EventSubWsListener) => {
+    const { users } = base.DM.getStreamStatus();
+    users.forEach((value) => {
+        eventSub.subscribeToStreamOnlineEvents(value.id, (event) => new TwitchStream(base, event).turnOnline());
+        eventSub.subscribeToStreamOfflineEvents(value.id, (event) => new TwitchStream(base, event).turnOffline());
+    });
+
+    eventSub.start();
+};
