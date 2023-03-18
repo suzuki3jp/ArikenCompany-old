5fac9ceedfb557a29842fc8b53825e76bcd2e13e
suzuki3jp <99058480+suzuki3jp@users.noreply.github.com>
2023-03-18 21:23:13 +0900

    :bug: buildエラーを解決 `Rollup failed to resolve import "@suzuki3jp/utils"`

--- pages/vite.config.js
+++ pages/vite.config.js
@@ -1,11 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), nodeResolve()],
 	build: {
 		rollupOptions: {
-			external: ['@suzuki3jp/utils', /node_modules\/@suzuki3jp\/utils/]
+			external: ['@suzuki3jp/utils']
 		}
 	}
 };
