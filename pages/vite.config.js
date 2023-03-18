960458245fb1de601b7385ffe429eff865fd617f
suzuki3jp <99058480+suzuki3jp@users.noreply.github.com>
2023-03-18 21:53:41 +0900

    :zap: 不要なconfigを削除

--- pages/vite.config.js
+++ pages/vite.config.js
@@ -1,14 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit(), nodeResolve()],
-	build: {
-		rollupOptions: {
-			external: ['@suzuki3jp/utils', /node_modules\/@suzuki3jp/]
-		}
-	}
+	plugins: [sveltekit()]
 };
 
 export default config;
