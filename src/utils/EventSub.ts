dcb232e17624b1943316d36426eb27b0e6590c48
suzuki3jp <suzuki3.info@gmail.com>
2023-03-31 05:14:40 +0900

    :recycle: 既存のログを英語に変更

--- src/utils/EventSub.ts
+++ src/utils/EventSub.ts
@@ -3,10 +3,10 @@ import { TwitchStream } from '../class/TwitchStream';
 
 export const subscribeOnlineEvent = (base: Base, id: string) => {
     base.eventSub.subscribeToStreamOnlineEvents(id, (event) => new TwitchStream(base, event).turnOnline());
-    base.emitDebug(`${id}の配信オンラインeventSubをリッスン`);
+    base.logger.info(`Listening stream event-sub ${id}`);
 };
 
 export const subscribeOfflineEvent = (base: Base, id: string) => {
     base.eventSub.subscribeToStreamOfflineEvents(id, (event) => new TwitchStream(base, event).turnOffline());
-    base.emitDebug(`${id}の配信オフラインeventSubをリッスン`);
+    base.logger.info(`Listening stream event-sub ${id}`);
 };
