36a0daf770d098f95773cef02b1dddd61b59ed78
suzuki3jp <99058480+suzuki3jp@users.noreply.github.com>
2023-04-12 12:50:07 +0900

    :fire: 自動デプロイ化によって不必要になったファイルを削除

--- dist/events/twitch/ready.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.twitchReady = void 0;
-const twitchReady = (base) => {
-    base.logger.info(`Connected twitch chat to ${base.DM.getSettings().twitch.channels.join(', ')}`);
-    base.logger.system('twitch client is ready.');
-};
-exports.twitchReady = twitchReady;
