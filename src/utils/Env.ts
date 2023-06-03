57082257107cf1562cbc5620b98c80a9fb1d16b6
suzuki3jp <99058480+suzuki3jp@users.noreply.github.com>
2023-06-03 14:14:15 +0900

    :zap: .envの読み込み方を変更

---
+++ src/utils/Env.ts
@@ -0,0 +1,18 @@
+import { config } from 'dotenv';
+config();
+
+import { DotEnv } from '../class/JsonTypes';
+
+export const dotenv = (): DotEnv => {
+    const { TWITCH_TOKEN, TWITCH_REFRESHTOKEN, TWITCH_CLIENTID, TWITCH_CLIENTSECRET, DISCORD_TOKEN } = process.env;
+
+    if (!TWITCH_TOKEN) throw envError('TWITCH_TOKEN');
+    if (!TWITCH_CLIENTID) throw envError('TWITCH_REFRESHTOKEN');
+    if (!TWITCH_CLIENTSECRET) throw envError('TWITCH_CLIENTSECRET');
+    if (!TWITCH_REFRESHTOKEN) throw envError('TWITCH_REFRESHTOKEN');
+    if (!DISCORD_TOKEN) throw envError('DISCORD_TOKEN');
+
+    return { TWITCH_CLIENTID, TWITCH_CLIENTSECRET, TWITCH_REFRESHTOKEN, TWITCH_TOKEN, DISCORD_TOKEN };
+};
+
+const envError = (env: string): Error => new Error(`${env} not found in .env file.`);
