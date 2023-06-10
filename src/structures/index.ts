1027788a380136f1ff6b1e7d82e6076752eac272
suzuki3jp <99058480+suzuki3jp@users.noreply.github.com>
2023-06-11 07:38:44 +0900

    :zap: `DotEnv`を定義

---
+++ src/structures/index.ts
@@ -0,0 +1,1 @@
+export { DotEnv } from './DotEnv';
---
+++ src/typings/index.ts
@@ -0,0 +1,8 @@
+export interface DotEnvData {
+    DISCORD_TOKEN: string;
+    TWITCH_CLIENTID: string;
+    TWITCH_CLIENTSECRET: string;
+    TWITCH_REFRESHTOKEN: string;
+    TWITCH_TOKEN: string;
+    TWITCH_HTTPSECRET: string;
+}
