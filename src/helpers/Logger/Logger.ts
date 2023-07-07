c872a8182cd6cf97371dbd37fa41c549f69bdf21
suzuki3jp <99058480+suzuki3jp@users.noreply.github.com>
2023-07-08 00:54:06 +0900

    :zap: Loggerを作成

---
+++ src/helpers/Logger/Logger.ts
@@ -0,0 +1,54 @@
+import { JST } from '@/helpers/Date/index';
+
+export class Logger {
+    public parent: Logger | null;
+
+    constructor(public name: string, parent?: Logger) {
+        this.parent = parent ?? null;
+    }
+
+    public createChild(name: string): Logger {
+        return new Logger(name, this);
+    }
+
+    public error(...messages: string[]) {
+        this.log('error', ...messages);
+    }
+
+    public system(...messages: string[]) {
+        this.log('system', ...messages);
+    }
+
+    public info(...messages: string[]) {
+        this.log('info', ...messages);
+    }
+
+    public debug(...messages: string[]) {
+        this.log('debug', ...messages);
+    }
+
+    private log(level: LogLevel, ...messages: string[]) {
+        console.log(this.makeMessage(level, ...messages));
+    }
+
+    private makeMessage(level: LogLevel, ...messages: string[]): string {
+        let parents: Logger[] = [];
+        let child: Logger = this;
+
+        while (child.parent) {
+            parents.unshift(child.parent);
+            child = child.parent;
+        }
+
+        return [`[${new JST().toString()} JST]`, `[${[...parents, this].map((i) => i.name).join('/')}]`, `[${level.toUpperCase()}]`, ...messages].join(' ');
+    }
+}
+
+type LogLevel = 'error' | 'system' | 'info' | 'debug';
+
+export const LogMessages = {
+    startingArikenCompany: 'Starting ArikenCompany...',
+    startedArikenCompany: 'Started ArikenCompany.',
+    registerdApiRoutes: 'Registerd ArikenCompany api routes.',
+    startedApi: (port: number) => `Started ArikenCompany api. listening port: ${port}`,
+};
