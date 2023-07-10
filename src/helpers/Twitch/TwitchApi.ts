de4c09127ea67c2ab7b17359cd44b94081af9ac0
suzuki3jp <99058480+suzuki3jp@users.noreply.github.com>
2023-07-11 04:08:37 +0900

    :zap: `TwitchApi`に`getUserById()`を追加

--- src/helpers/Twitch/TwitchApi.ts
+++ src/helpers/Twitch/TwitchApi.ts
@@ -1,9 +1,13 @@
-import { ApiClient } from '@twurple/api';
+import { ApiClient, HelixUser } from '@twurple/api';
 
 import { TwitchAuth } from '@/helpers/Twitch/TwitchAuth';
 
 export class TwitchApi extends ApiClient {
     constructor(public auth: TwitchAuth) {
         super({ authProvider: auth });
+    }
+
+    async getUserById(id: string): Promise<HelixUser | null> {
+        return await this.users.getUserById(id);
     }
 }
