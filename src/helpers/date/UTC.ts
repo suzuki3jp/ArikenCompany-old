883c7a4375141efe6ccdc1cedc335f1e04bbdf0a
suzuki3jp <99058480+suzuki3jp@users.noreply.github.com>
2023-07-11 03:36:31 +0900

    :zap: パスのミスを修正

--- src/helpers/date/UTC.ts
+++ src/helpers/date/UTC.ts
@@ -2,7 +2,7 @@ import dayjs from 'dayjs';
 import tz from 'dayjs/plugin/timezone';
 import utc from 'dayjs/plugin/utc';
 
-import { BaseDate } from '@/helpers/Date/BaseDate';
+import { BaseDate } from '@/helpers/date/BaseDate';
 
 // load dayjs plugins
 dayjs.extend(tz);
